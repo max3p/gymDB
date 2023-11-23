14a836961155835f34a25f82f8c5c2bae2ff3efc
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-11-23 15:29:54 -0700

    Made the web app look pretty

--- start-server.bat
+++ start-server.bat
@@ -1,6 +1,11 @@
 @echo off
 cd /d "%~dp0back-end"
 start /B cmd /c "node dbDriver.js ^& pause"
+
 cd /d "%~dp0front-end"
 start /B cmd /c "npx http-server --cors -o /index.html ^& pause"
-echo Visit address http://localhost:8080/ in your web browser
+
+:: Open the default web browser with the URL
+start http://localhost:8080/
+
+
