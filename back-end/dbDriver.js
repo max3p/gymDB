6bc7bfc2e8168f9ac186b688e3c5679bda5086e9
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-11-29 20:18:05 -0700

    search button added in login.html

--- back-end/dbDriver.js
+++ back-end/dbDriver.js
@@ -1,99 +1,43 @@
-// dbDriver.js
+/*
+dbDriver.js
+Handles connecting  to MySQL database
+*/
 
-const createDB = require('./dbCreator');
 const mysql = require('mysql2');
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = 3000;
 
-// Set the flag to true if you want to initialize a new database
-const initializeNewDB = false;
-
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "pass",
-    database: "gymdb"   //this won't exist if you have not yet initiaized a db, may need to comment out
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: 'pass',
+  database: 'gymdb'
 });
 
-app.use(cors());
-app.use(express.json());
-
-// Connect to the DB
-con.connect(function (err) {
+// Function to execute a query with a connection from the pool
+const executeQuery = (sql, params, callback) => {
+    console.log('dbDriver.js: query called');
+    pool.getConnection((err, connection) => {
     if (err) {
-        console.error('dbDriver: Error connecting to the database:', err);
-        return;
+      console.error('Error getting MySQL connection:', err);
+      return callback(err, null);
     }
-    console.log("dbDriver: Connected to the database");
+    console.log('dbDriver.js: connected to database');
+    
 
-    // Initialize a new 'gymdb' if not already initialized
-    if (initializeNewDB) {
-        createDB(function () {
-            con.config.database = "gymdb";
-            console.log("dbDriver: con.database updated to 'gymdb'");
-        });
-    }
+    connection.query(sql, params, (queryErr, results) => {
+      connection.release(); // Release the connection back to the pool
 
-    // Start the server after connecting to the database
-    app.listen(port, () => {
-        console.log(`dbDriver: Server is running on port ${port}`);
+      if (queryErr) {
+        console.error('Error executing MySQL query:', queryErr);
+        return callback(queryErr, null);
+      }
+
+      callback(null, results);
     });
-});
+  });
+};
 
-// Callback to be executed after connecting and selecting the database
-function onDatabaseConnected(callback) {
-    con.connect(function (err) {
-        if (err) {
-            console.error('dbDriver: Error connecting to the database:', err);
-            return;
-        }
-        console.log("dbDriver: Connected to the database");
-
-        // Initialize a new 'gymdb' if not already initialized
-        if (initializeNewDB) {
-            createDB(function () {
-                con.config.database = "gymdb";
-                console.log("dbDriver: con.database updated to 'gymdb'");
-                callback(); // Invoke the callback after selecting the database
-            });
-        } else {
-            callback(); // Invoke the callback without initializing the database
-        }
-    });
-}
-
-// Queries MySQL DB
-// Input: the query as a string
-function queryDB(query, callback) {
-    onDatabaseConnected(function () {
-        con.query(query, function (err, result) {
-            if (err) {
-                console.error('Error executing query:', err);
-                if (callback) {
-                    callback({ error: 'An error occurred while executing the query.' });
-                }
-            } else {
-                if (callback) {
-                    callback(result);
-                }
-            }
-        });
-    });
-}
-
-// Expose the queryDB function
-app.post('/query', (req, res) => {
-    const { query } = req.body;
-    queryDB(query, (result) => {
-        res.json(result);
-    });
-});
-
-// Start the server after connecting to the database
-app.listen(port, 'localhost', () => {
-    console.log(`dbDriver: Server is running on port ${port}`);
-});
-
-module.exports = { queryDB };
+module.exports = {
+  executeQuery
+};
--- start-server.bat
+++ start-server.bat
@@ -1,9 +1,8 @@
 @echo off
-cd /d "%~dp0back-end"
-start /B cmd /c "node dbDriver.js ^& pause"
 
-cd /d "%~dp0front-end"
-start /B cmd /c "npx http-server --cors -o /index.html ^& pause"
+start cmd /c "cd /d front-end && npx http-server --cors -o /index.html"
+
+start cmd /c "cd /d back-end && node api.js"
 
 :: Open the default web browser with the URL
 :: start http://localhost:8080/
