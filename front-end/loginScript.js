1c0efa1ced01aaf8de685df68729869697f30399
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-12-04 20:08:16 -0700

    made login work with username and password

--- front-end/loginScript.js
+++ front-end/loginScript.js
@@ -1,17 +1,27 @@
 // loginScript.js
 
-// Get the modal
-var modal = document.getElementById('id01');
-var sModal = document.getElementById('id02');
-var suModal = document.getElementById('id04');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    } else if (event.target == sModal) {
-        sModal.style.display = "none";
-    } else if (event.target == suModal) {
-        suModal.style.display = "none";
+document.addEventListener('DOMContentLoaded', function () {
+    var loginButton = document.getElementById('id05');
+    if (loginButton) {
+        loginButton.addEventListener('click', function () {
+            loginUser();
+        });
     }
-};
+});
+
+function loginUser() {
+    var hardcodedUsername = 'user';
+    var hardcodedPassword = 'pass';
+
+    // Get the entered username and password
+    var username = document.getElementsByName('uname')[0].value;
+    var password = document.getElementsByName('psw')[0].value;
+
+    if (username === hardcodedUsername && password === hardcodedPassword) {
+        // Redirect to another page if username and password match
+        window.location.href = 'index.html';
+    } else {
+        // Display a message if username or password doesn't match
+        alert('Invalid username or password. Please try again.');
+    }
+}
