7f4d8348b832b9ac52d510e1250c958c3ca77d8b
delaram <delaram.bahreiniesfa@ucalgary.ca>
2023-12-10 22:39:58 -0700

    icon click with pop up window for members

--- front-end/members.js
+++ front-end/members.js
@@ -31,9 +31,18 @@ const membersList = [
       const member = membersList[i];
       const row = table.insertRow();
 
-      // Add user icon to the first cell
+      // Add user icon to the first cell and make it clickable
       const iconCell = row.insertCell(0);
-      iconCell.innerHTML = '<i class="glyphicon glyphicon-user" style="font-size:24px;"></i>';
+      const icon = document.createElement('i');
+      icon.className = 'glyphicon glyphicon-user clickable-icon';
+      icon.style.fontSize = '24px';
+      icon.addEventListener('click', function() {
+        // Open the modal with member information
+        const modalBody = document.getElementById('memberModalBody');
+        modalBody.innerHTML = `<h2>${member.firstName} ${member.lastName}</h2><p>ID: ${member.id}</p>`;
+        $('#memberModal').modal('show');
+      });
+      iconCell.appendChild(icon);
 
       row.insertCell(1).textContent = member.id;
       row.insertCell(2).textContent = member.firstName;
