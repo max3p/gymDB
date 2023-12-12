1e16f21fed3e6224a88a6a2d229799d7c863e60b
delaram <delaram.bahreiniesfa@ucalgary.ca>
2023-12-11 19:10:15 -0700

    remove icon added

--- front-end/members.js
+++ front-end/members.js
@@ -1,3 +1,4 @@
+// const api = require("../back-end/api.js");
 // Sample list of objects representing members
 const membersList = [
     {
@@ -28,73 +29,76 @@ const membersList = [
     // Add more members as needed
   ];
   
-  // Accessing and displaying information in the table
   document.addEventListener('DOMContentLoaded', function () {
     const table = document.querySelector('table');
-
-for (let i = 0; i < membersList.length; i++) {
-  const member = membersList[i];
-  const row = table.insertRow();
-
-  // Add user icon to the first cell and make it clickable
-  const iconCell = row.insertCell(0);
-  const icon = document.createElement('i');
-  icon.className = 'glyphicon glyphicon-user clickable-icon';
-  icon.style.fontSize = '24px';
-  icon.addEventListener('click', function() {
-    // Open the modal with member information
-    const modalBody = document.getElementById('memberModalBody');
-    modalBody.innerHTML = ''; // Clear previous content
-
-    // Create HTML elements for member details
-    const detailsContainer = document.createElement('div');
-    detailsContainer.innerHTML = `<h2>${member.firstName} ${member.lastName}</h2><ul>`;
-
-    // Loop through the attributes of the member
-    for (const key in member) {
-      if (member.hasOwnProperty(key)) {
-        detailsContainer.innerHTML += `<li>${key}: ${member[key]}</li>`;
-      }
-    }
-    detailsContainer.innerHTML += `</ul>`;
-
-    // Create HTML elements for member details
-    // const detailsContainer = document.createElement('div');
-    // detailsContainer.innerHTML = `
-    //   <h2>${member.firstName} ${member.lastName}</h2>
-    //   <p>ID: ${member.id}</p>
-    //   <p>Age: ${member.age}</p>
-    //   <p>Email: ${member.email}</p>
-    //   <!-- Add more details as needed -->
-    // `;
-
-    // Append details to the modal body
-    modalBody.appendChild(detailsContainer);
-
-    // Show the modal
-    $('#memberModal').modal('show');
-  });
-  iconCell.appendChild(icon);
   
-        // Add other cells
-        row.insertCell(1).textContent = member.id;
-        row.insertCell(2).textContent = member.firstName;
-        row.insertCell(3).textContent = member.lastName;
-      }
+    for (let i = 0; i < membersList.length; i++) {
+      const member = membersList[i];
+      const row = table.insertRow();
+  
+      // Add user icon to the first cell and make it clickable
+      const iconCell = row.insertCell(0);
+      const icon = document.createElement('i');
+      icon.className = 'fas fa-user clickable-icon';
+      icon.style.fontSize = '24px';
+      icon.addEventListener('click', function() {
+        // Open the modal with member information
+        const modalBody = document.getElementById('memberModalBody');
+        modalBody.innerHTML = ''; // Clear previous content
+  
+        // Create HTML elements for member details
+        const detailsContainer = document.createElement('div');
+        detailsContainer.innerHTML = `<h2>${member.firstName} ${member.lastName}</h2><ul>`;
+  
+        // Loop through the attributes of the member
+        for (const key in member) {
+          if (member.hasOwnProperty(key)) {
+            detailsContainer.innerHTML += `<li>${key}: ${member[key]}</li>`;
+          }
+        }
+        detailsContainer.innerHTML += `</ul>`;
+  
+        // Append details to the modal body
+        modalBody.appendChild(detailsContainer);
+  
+        // Show the modal
+        $('#memberModal').modal('show');
+      });
+      iconCell.appendChild(icon);
+  
+      // Add trash icon to the last cell and make it clickable
+      const trashIconCell = row.insertCell(-1);
+      const trashIcon = document.createElement('i');
+      trashIcon.className = 'fas fa-trash clickable-icon';
+      trashIcon.style.fontSize = '24px';
+      trashIcon.addEventListener('click', function () {
+        // Handle the logic to remove the member or show a confirmation modal
+        console.log('Remove member clicked:', member);
+      });
+      trashIconCell.appendChild(trashIcon);
+  
+      // Add other cells
+      row.insertCell(1).textContent = member.id;
+      row.insertCell(2).textContent = member.firstName;
+      row.insertCell(3).textContent = member.lastName;
+    }
   });
+  
+
+  
+
+  
 
   var suModal = document.getElementById('id21');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    } else if (event.target == sModal) {
-        sModal.style.display = "none";
-    } else if (event.target == suModal) {
+    if (event.target == suModal) {
         suModal.style.display = "none";
     }
 };
+
+  
 function navigateToPage() {
 
     window.location.href = "login.html";
@@ -106,3 +110,39 @@ function navigateToPage() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+// Assuming you have an API object that contains the addMember function
+
+  // Find the form in your HTML (add an ID to the form if not present)
+const signupForm = document.querySelector('#signupForm');
+
+// Listen for the form submission event
+signupForm.addEventListener('submit', function (event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Get user input from the form fields
+    const name = document.querySelector('input[name="Name"]').value;
+    const phoneNumber = document.querySelector('input[name="phoneNumber"]').value;
+    const address = document.querySelector('input[name="Address"]').value;
+    const card = document.querySelector('input[name="Card"]').value;
+    const ePhone = document.querySelector('input[name="ePhone"]').value;
+
+    // Create a new member object
+    const newMember = {
+        name: name,
+        phone_number: phoneNumber,
+        emergency_contact: ePhone,
+        address: address,
+        card: card
+        // Add other properties as needed
+    };
+
+    // Call addMember to insert the new member into the db
+    api.addMember(newMember, (err) => {
+        if (err) {
+            console.error('Error:', err);
+        } else {
+            console.log('\nInserted new Member into the db');
+        }
+    });
+});
