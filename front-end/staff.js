ef7aae7abe73be4d120547d30ebb8a8f6428d08e
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-12-12 12:29:19 -0700

    fetched staff from db onto webpage

--- front-end/staff.js
+++ front-end/staff.js
@@ -3,38 +3,53 @@ staff.js
 Populates the table in staff.html
 */
 
-// Get list of all staff in db
+const baseURL = 'http://localhost:3000/api';
 
 // User Icon
 var imageUrl = "./images/user-icon.png";
 
-// test data
-var staffList = [
-    { employee_id: "001", name: "John Doe" },
-    { employee_id: "002", name: "Jane Smith" },
-    { employee_id: "003", name: "Bob Johnson" }
-];
+async function fetchData() {
+    try {
+        // Set up variables
+        const entityNameToRetrieve = 'Worker';
+        const response = await fetch(`${baseURL}/${entityNameToRetrieve}`);
+        const staffList = await response.json();
 
-// Get the table by ID
-var table = document.getElementById("staffTable");
+        // Iterate through each tuple in the array and print for debug
+        console.log(`Data retrieved from database:`);
+        staffList.forEach((tupleObject) => {
+            console.log('-------------------');
+            console.log('   Staff ID:', tupleObject.employee_id);
+            console.log('   Name:', tupleObject.name);
+        });
 
-// Iterate through the data and create rows
-for (var i = 0; i < staffList.length; i++) {
-    // Create a new row
-    var row = table.insertRow(i);
+        // Get the table by ID
+        var table = document.getElementById("staffTable");
 
-    // Insert an image into the leftmost cell
-    var imgCell = row.insertCell(0);
-    var img = document.createElement("img");
-    img.src = imageUrl;
-    img.alt = "Image";
-    imgCell.appendChild(img);
+        // Iterate through the data and create rows
+        for (var i = 0; i < staffList.length; i++) {
+            // Create a new row
+            var row = table.insertRow(i);
 
-    // Insert employee_id into the second cell
-    var employeeIdCell = row.insertCell(1);
-    employeeIdCell.innerHTML = staffList[i].employee_id;
+            // Insert an image into the leftmost cell
+            var imgCell = row.insertCell(0);
+            var img = document.createElement("img");
+            img.src = imageUrl;
+            img.alt = "Image";
+            imgCell.appendChild(img);
 
-    // Insert name into the third cell
-    var nameCell = row.insertCell(2);
-    nameCell.innerHTML = staffList[i].name;
+            // Insert employee_id into the second cell
+            var employeeIdCell = row.insertCell(1);
+            employeeIdCell.innerHTML = staffList[i].employee_id;
+
+            // Insert name into the third cell
+            var nameCell = row.insertCell(2);
+            nameCell.innerHTML = staffList[i].name;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
+
+// Call async function
+fetchData();
