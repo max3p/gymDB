83c7673d68db6ce7a9826b655e7f173a6bc5823f
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-12-12 12:11:15 -0700

    made staff.js table use an object list

--- front-end/staff.js
+++ front-end/staff.js
@@ -5,25 +5,36 @@ Populates the table in staff.html
 
 // Get list of all staff in db
 
-// Sample data
-var data = [
-    ["Data 1", "Data 2", "Data 3", "Data 4"],
-    ["Data 1", "Data 2", "Data 3", "Data 4"],
-    ["Data 1", "Data 2", "Data 3", "Data 4"]
+// User Icon
+var imageUrl = "./images/user-icon.png";
+
+// test data
+var staffList = [
+    { employee_id: "001", name: "John Doe" },
+    { employee_id: "002", name: "Jane Smith" },
+    { employee_id: "003", name: "Bob Johnson" }
 ];
 
 // Get the table by ID
 var table = document.getElementById("staffTable");
 
 // Iterate through the data and create rows
-for (var i = 0; i < data.length; i++) {
+for (var i = 0; i < staffList.length; i++) {
     // Create a new row
     var row = table.insertRow(i);
 
-    // Iterate through the data for each row and create cells
-    for (var j = 0; j < data[i].length; j++) {
-        // Create a new cell and set its content
-        var cell = row.insertCell(j);
-        cell.innerHTML = data[i][j];
-    }
+    // Insert an image into the leftmost cell
+    var imgCell = row.insertCell(0);
+    var img = document.createElement("img");
+    img.src = imageUrl;
+    img.alt = "Image";
+    imgCell.appendChild(img);
+
+    // Insert employee_id into the second cell
+    var employeeIdCell = row.insertCell(1);
+    employeeIdCell.innerHTML = staffList[i].employee_id;
+
+    // Insert name into the third cell
+    var nameCell = row.insertCell(2);
+    nameCell.innerHTML = staffList[i].name;
 }
