d18c4e6402eb579406c1eca01f24a80dc937eeee
mpagels3022 <101290617+mpagels3022@users.noreply.github.com>
2023-12-12 14:58:04 -0700

    added data fetch to worker modal

--- front-end/staff.js
+++ front-end/staff.js
@@ -8,20 +8,13 @@ const baseURL = 'http://localhost:3000/api';
 // User Icon
 var imageUrl = "./images/user-icon.png";
 
-async function fetchData() {
+async function fetchAllData() {
     try {
         // Set up variables
         const entityNameToRetrieve = 'Worker';
         const response = await fetch(`${baseURL}/${entityNameToRetrieve}`);
         const staffList = await response.json();
-
-        // Iterate through each tuple in the array and print for debug
-        console.log(`Data retrieved from database:`);
-        staffList.forEach((tupleObject) => {
-            console.log('-------------------');
-            console.log('   Staff ID:', tupleObject.employee_id);
-            console.log('   Name:', tupleObject.name);
-        });
+        console.log(`Data retrieved from database`);
 
         // Get the table by ID
         var table = document.getElementById("staffTable");
@@ -59,7 +52,14 @@ async function fetchData() {
 
             // Check if the clicked element is a "Expand" button
             if (target.tagName === 'BUTTON' && target.innerHTML === 'Expand') {
-                expand();
+                // Find the corresponding row
+                var rowIndex = target.closest('tr').rowIndex;
+
+                // Get the employee_id from the second cell of the clicked row
+                var employeeId = table.rows[rowIndex].cells[1].innerHTML;
+
+                // Call the expand function with the employee_id
+                expand(employeeId);
             }
         });
 
@@ -69,9 +69,44 @@ async function fetchData() {
 }
 
 // Expand function
-function expand() {
-    console.log(""); //print member name
+async function expand(employeeID) {
+    console.log("Expanding employee ID:", employeeID);
+
+    const entityName = 'Worker';
+    fetch(`${baseURL}/${entityName}/${employeeID}`)
+        .then((response) => response.json())
+        .then((tupleObject) => {
+            console.log('\ngetTuple() Result:');
+            openModal(tupleObject.employee_id);
+        })
+        .catch((error) => console.error('Error:', error));
 }
 
-// Call async function
-fetchData();
+// Function to open modal and display employee ID
+function openModal(employeeId) {
+    var modal = document.getElementById("expandModal");
+    var employeeIdInModal = document.getElementById("employeeIdInModal");
+
+    // Set the employee ID in the modal content
+    employeeIdInModal.textContent = "Employee ID: " + employeeId;
+
+    // Display the modal
+    modal.style.display = "block";
+}
+
+// Function to close the modal
+function closeModal() {
+    var modal = document.getElementById("expandModal");
+    modal.style.display = "none";
+}
+
+// Close the modal if the user clicks outside of it
+window.onclick = function (event) {
+    var modal = document.getElementById("expandModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// Call async function to fetch data
+fetchAllData();
